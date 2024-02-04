46967ec666dfeb6e67917f44e8141d8bb63e21ef
Camden <60766780+CamCalDIAMOND@users.noreply.github.com>
2024-02-04 12:24:36 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
@@ -5,3 +6,22 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+startingPoints = 1000
+
+
+people = {   
+   "jess": startingPoints,
+   "lily": startingPoints,
+   "logan": startingPoints,
+   "camden": startingPoints
+}
+
+people["camden"] = people["camden"] + 10
+
+
+file_path = "./data.json"
+json_string = json.dumps(people,indent=4)
+
+with open(file_path, 'w') as json_file:
+   json_file.write(json_string)
